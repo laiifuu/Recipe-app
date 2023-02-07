626f319213768846d058c6bb85d6c3606828572d
Daron976 <daronkwiafe@gmail.com>
2023-02-07 19:16:22 +0200

    Fix issues with add food item

--- db/migrate/20230207065307_add_default_to_measurement_unit.rb
+++ db/migrate/20230207065307_add_default_to_measurement_unit.rb
@@ -1,5 +1,5 @@
 class AddDefaultToMeasurementUnit < ActiveRecord::Migration[7.0]
   def change
-    change_column_default :foods, :measurement_unit, from: nil, to: 'unknown measurement unit'
+    change_column_default :foods, :measurement_unit, from: nil, to: 'unit'
   end
 end

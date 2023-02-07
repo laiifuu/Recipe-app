91f175c8a6022940e85388aa5dfbda67b76ae792
Hayet Fellah <hayet.fellah145@gmail.com>
2023-02-07 17:15:54 +0100

    Add username to the recipe

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,5 +1,5 @@
 class PublicRecipesController < ApplicationController
   def index
-    @public_recipes = Recipe.where(public: true).order('created_at DESC')
+    @public_recipes = Recipe.includes(:user).where(public: true).order('created_at DESC')
   end
 end

16814f1572d7ea561a01dbadf8d6678999d7423f
Hayet Fellah <hayet.fellah145@gmail.com>
2023-02-07 13:52:06 +0100

    Fix linter errors

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,10 @@
 class RecipesController < ApplicationController
-  def index 
+  def index
     @user = current_user
-    @recipes = @user.recipes #I dont know how to add the n+1 thingy x) 
+    @recipes = @user.recipes # I dont know how to add the n+1 thingy x)
   end
 
-  def show 
+  def show
     @recipe = Recipe.find(params[:id])
   end
 
@@ -16,21 +16,21 @@ class RecipesController < ApplicationController
     recipe = Recipe.new(recipe_params)
     recipe.user = current_user
     if recipe.save
-      flash[:success] = "New recipe created."
+      flash[:success] = 'New recipe created.'
       redirect_to recipes_path
-    else 
+    else
       render :new
     end
   end
 
-  def destroy 
+  def destroy
     Recipe.find(params[:id]).destroy
     redirect_to recipes_path
   end
 
-  private 
+  private
 
-  def recipe_params 
+  def recipe_params
     params.require(:recipe).permit(:name, :description, :preparation_time, :cooking_time, :public)
   end
 end

b5347281542897d28105f4a34abcfaf0cabe20f3
Daron976 <daronkwiafe@gmail.com>
2023-02-08 15:14:02 +0200

    altered association

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,7 @@
 class Food < ApplicationRecord
   has_many :inventory_food, foreign_key: 'food_id', dependent: :delete_all
-  has_many :recipe_foods
+  # has_many :recipe_foods
+  has_many :recipe_foods, foreign_key: 'food_id', dependent: :delete_all
   has_many :recipes, through: :recipe_foods
 
   validates :name, presence: true

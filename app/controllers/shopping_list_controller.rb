d7ab0b9e38415abc8eda7b1fdad30b48294eb3f0
vflorin1995 <vflorin1995@gmail.com>
2023-02-09 12:49:34 +0200

    Fix Shopping list controller bug

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,40 +1,24 @@
 class ShoppingListController < ApplicationController
     def index
-        @arr1 = []
-        @arr2 = []
-        @arr3 = []
-
-        @food_of_recipe = RecipeFood.includes(:food).where(recipe_id: params[:recipe_id])
-        @food_of_recipe.each do |food|
-            @arr2 << {name: food.food.name, quantity: food.quantity, price: food.food.price, original_id: food.food.id}
-        end
+        @final = []
 
         @food_of_inventory = InventoryFood.includes(:food).where(inventory_id: params[:inventory_id])
-        @food_of_inventory.each do |food|
-            @arr1 << {name: food.food.name, quantity: food.quantity, price: food.food.price, original_id: food.food.id}
-        end
+        @food_of_recipe = RecipeFood.includes(:food).where(recipe_id: params[:recipe_id])
 
-        @arr1.each do |item1|
-            @arr2.each do |item2|
-                if item1[:name] == item2[:name]
-                    if item1[:quantity] >= item2[:quantity]
-                        @arr1.delete(item1)
-                        @arr2.delete(item1)
-                    else
-                        final_quantity = item2[:quantity] - item1[:quantity]
-                        @arr2 << {name: item1[:name], quantity: final_quantity, price: item1[:price], original_id: item1[:id]}
-                        @arr1.delete(item1)
-                        @arr2.delete(item2)
+        @food_of_recipe.each do |food1|
+            k=0
+            @food_of_inventory.each do |food2|
+                if food1.food.name == food2.food.name
+                    k=1
+                    if food2.quantity < food1.quantity
+                        final_quantity = food1.quantity - food2.quantity
+                        @final << {name: food2.food.name, quantity: final_quantity, price: food2.food.price}
                     end
                 end
             end
+            if k==0
+                @final << {name: food1.food.name, quantity: food1.quantity, price: food1.food.price}
+            end
         end
-
-        @total = 0
-        @arr2.each do |item3|
-            @total += item3[:quantity] * item3[:price]
-        end
-
-
     end
 end

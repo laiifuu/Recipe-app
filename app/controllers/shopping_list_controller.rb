a9253b16bd96e338339c302622914166deb28248
vflorin1995 <vflorin1995@gmail.com>
2023-02-09 12:50:13 +0200

    Fix linters

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,24 +1,22 @@
 class ShoppingListController < ApplicationController
-    def index
-        @final = []
+  def index
+    @final = []
 
-        @food_of_inventory = InventoryFood.includes(:food).where(inventory_id: params[:inventory_id])
-        @food_of_recipe = RecipeFood.includes(:food).where(recipe_id: params[:recipe_id])
+    @food_of_inventory = InventoryFood.includes(:food).where(inventory_id: params[:inventory_id])
+    @food_of_recipe = RecipeFood.includes(:food).where(recipe_id: params[:recipe_id])
 
-        @food_of_recipe.each do |food1|
-            k=0
-            @food_of_inventory.each do |food2|
-                if food1.food.name == food2.food.name
-                    k=1
-                    if food2.quantity < food1.quantity
-                        final_quantity = food1.quantity - food2.quantity
-                        @final << {name: food2.food.name, quantity: final_quantity, price: food2.food.price}
-                    end
-                end
-            end
-            if k==0
-                @final << {name: food1.food.name, quantity: food1.quantity, price: food1.food.price}
-            end
+    @food_of_recipe.each do |food1|
+      k = 0
+      @food_of_inventory.each do |food2|
+        next unless food1.food.name == food2.food.name
+
+        k = 1
+        if food2.quantity < food1.quantity
+          final_quantity = food1.quantity - food2.quantity
+          @final << { name: food2.food.name, quantity: final_quantity, price: food2.food.price }
         end
+      end
+      @final << { name: food1.food.name, quantity: food1.quantity, price: food1.food.price } if k.zero?
     end
+  end
 end
